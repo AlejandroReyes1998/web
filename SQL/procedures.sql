b94d6e61a0d3b1b020d668cebf2c37d20496751a
Uriel12009DX <crhs@gft.com>
2020-12-20 16:52:16 -0600

    leakey

--- SQL/procedures.sql
+++ SQL/procedures.sql
@@ -20,8 +20,8 @@ BEGIN
 	set existe_username = (select count(*) from Usuario where nombreUsuario = sp_username);
     set existe_cedula = (select count(*) from Medico where cedula = sp_cedula);
     set existe_correo = (select count(*) from Medico where correo = sp_correo);
-    IF existe_username = 0 THEN 
-		IF existe_cedula = 0 THEN 
+    IF existe_username = 0 THEN
+		IF existe_cedula = 0 THEN
 			IF existe_correo = 0 THEN
 				insert into Persona (nombre,apaterno,amaterno,IDCatalogoPersona) values (sp_nombre,sp_apaterno,sp_amaterno,1);
 				insert into Usuario (nombreUsuario,passwor_d,IDPersona,IDCatalogoUsuario) values (sp_username,sha2(sp_password,0),
@@ -32,7 +32,7 @@ BEGIN
 			ELSE
 				SELECT 'El correo electrónico introducido ya se encuentra registrado.' AS MSJ;
 			END IF;
-		ELSE 
+		ELSE
 			SELECT 'La cédula introducida ya se encuentra registrada.' AS MSJ;
 		END IF;
     ELSE
@@ -120,14 +120,15 @@ drop procedure if exists AltaNota;
 DELIMITER //
 CREATE PROCEDURE AltaNota(
 		IN sp_iv BLOB,
+		IN sp_leaK BLOB,
 		IN sp_Resumen_Interrogatorio NVARCHAR(280),
-		IN sp_Plan_Estudio NVARCHAR(280), 
-		IN sp_Pronostico NVARCHAR(280), 
-		IN sp_Exploracion_Fisica NVARCHAR(280), 
-		IN sp_Resultados_Estudios NVARCHAR(280), 
-		IN sp_Diagnostico_Problemas NVARCHAR(280), 
+		IN sp_Plan_Estudio NVARCHAR(280),
+		IN sp_Pronostico NVARCHAR(280),
+		IN sp_Exploracion_Fisica NVARCHAR(280),
+		IN sp_Resultados_Estudios NVARCHAR(280),
+		IN sp_Diagnostico_Problemas NVARCHAR(280),
 		IN sp_Estado_Mental NVARCHAR(280),
-		IN sp_Fecha NVARCHAR(10), 
+		IN sp_Fecha NVARCHAR(10),
         IN sp_Peso int,
 		IN sp_Talla int,
 		IN sp_Presion NVARCHAR(15),
@@ -138,9 +139,9 @@ CREATE PROCEDURE AltaNota(
 		IN sp_IDMedico int
 )
 BEGIN
-	insert into Signos (Peso,Talla,Presion,Frecuencia_Cardiaca,Frecuencia_Respiratorio,Temperatura) 
+	insert into Signos (Peso,Talla,Presion,Frecuencia_Cardiaca,Frecuencia_Respiratorio,Temperatura)
     values (sp_Peso,sp_Talla,sp_Presion,sp_Frecuencia_Cardiaca,sp_Frecuencia_Respiratorio,sp_Temperatura);
-	insert into NotaMedica (iv,Resumen_Interrogatorio,Plan_Estudio,Pronostico,Exploracion_Fisica,Resultados_Estudios,Diagnostico_Problemas,Estado_Mental,Fecha,IDPaciente,IDMedico,IDSignos) 
-    values (sp_iv,sp_Resumen_Interrogatorio,sp_Plan_Estudio,sp_Pronostico,sp_Exploracion_Fisica,sp_Resultados_Estudios,sp_Diagnostico_Problemas,sp_Estado_Mental,sp_Fecha,sp_IDPaciente,sp_IDMedico,(select MAX(idSignos) FROM Signos));
+	insert into NotaMedica (iv,leaK,Resumen_Interrogatorio,Plan_Estudio,Pronostico,Exploracion_Fisica,Resultados_Estudios,Diagnostico_Problemas,Estado_Mental,Fecha,IDPaciente,IDMedico,IDSignos)
+    values (sp_iv,sp_leaK,sp_Resumen_Interrogatorio,sp_Plan_Estudio,sp_Pronostico,sp_Exploracion_Fisica,sp_Resultados_Estudios,sp_Diagnostico_Problemas,sp_Estado_Mental,sp_Fecha,sp_IDPaciente,sp_IDMedico,(select MAX(idSignos) FROM Signos));
     SELECT 'REGISTRO EXITOSO' AS MSJ;
 END //
